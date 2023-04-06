d2f44c066808c0d1d1994c95d8dea581f7900f50
ahause23 <83376799+ahause23@users.noreply.github.com>
2023-04-07 07:49:00 +1000

    Update app.js

--- app/app.js
+++ app/app.js
@@ -139,9 +139,9 @@ async function readServerFirmwareFile(path, dispReadme = true)
         raw.send(null)
     })
 }
-const img = document.createElement("img");
-img.src = "https://picsum.photos/200/301";
-document.body.appendChild(img);
+//const img = document.createElement("img");
+//img.src = "https://picsum.photos/200/301";
+//document.body.appendChild(img);
 
 var app = new Vue({
     el: '#app',
@@ -197,7 +197,7 @@ var app = new Vue({
 			    	<p>On the small PCB enable COLORTONE Firmware Update by holding the BOOT button down, and then pressing, and releasing the RESET button.</p>
 				<ul>
 					<li><p>If the buttons were pressed in the correct order the flashing LED will turn off</p></li>
-					<li><p> img </p></li>
+					<li><p> <img src="pineapple.jpg" alt="Pineapple" style="width:170px;height:170px;"> </p></li>
 				</ul>
 			    </li>
                             <li><p>Click the Connect button at the top of this page.</p></li>
